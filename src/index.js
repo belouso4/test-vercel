7621a095799a507054be6f2143ddb6db08794de0
belouso4 <kibelousov495@proton.me>
2024-02-02 00:02:53 +0700

    interes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("web site is working")
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const ENVIRONMENT = process.env.NODE_ENV || '';
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.command('about', about());
+bot.on('message', greeting());
+
+//prod mode (Vercel)
+export const startVercel = async (req, res) => {
+  await production(req, res, bot);
+};
+//dev mode
+ENVIRONMENT !== 'production' && development(bot);
