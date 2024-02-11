bf4b698c65c452c6d1d935837560b00710d356b1
belouso4 <kibelousov495@proton.me>
2024-02-11 12:27:35 +0700

    fix vercel config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+import 'dotenv/config'
 console.log("web site is working")
+
+import {Telegraf} from 'telegraf'
+import {development, production} from "./core/index.js"
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.command('about', about());
-bot.on('message', greeting());
+bot.command('about', (ctx) => {
+  ctx.reply('234234 about')
+});
+bot.on('message', (ctx) => {
+  ctx.reply('234234 message: ' + ctx.message.text )
+});
 
 //prod mode (Vercel)
 export const startVercel = async (req, res) => {
